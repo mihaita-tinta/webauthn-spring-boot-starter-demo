2814a30fb86a2afe9e568258079c02bb0059d3ce
Mihaita Tinta <mihaita.tinta@ing.com>
2021-07-28 16:35:29 +0300

    all flows working

--- src/main/java/com/mih/webauthn/demo/PrivateResource.java
+++ src/main/java/com/mih/webauthn/demo/PrivateResource.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,16 +35,16 @@ public class PrivateResource {
     }
 
     @GetMapping("/devices")
-    public List<Map<String, String>> devices(@AuthenticationPrincipal WebAuthnUsernameAuthenticationToken token) {
+    public List<Map<String, String>> devices(Authentication token) {
         WebAuthnUser user = (WebAuthnUser) token.getPrincipal();
-        WebAuthnCredentials currentCredentials = token.getCredentials();
+        WebAuthnCredentials currentCredentials = (WebAuthnCredentials) token.getCredentials();
         log.debug("devices for user:  " + user);
         return credentialsRepository
                 .findAllByAppUserId(user.getId())
                 .stream()
                 .map(credentials ->
                         Map.of("id", credentials.getId().toString(),
-                                "userAgent", credentials.getUserAgent(),
+                                "userAgent", credentials.getUserAgent() == null ? "N/A" : credentials.getUserAgent() ,
                                 "currentDevice", "" + currentCredentials.getId().equals(credentials.getId())))
                 .collect(Collectors.toList());
     }
